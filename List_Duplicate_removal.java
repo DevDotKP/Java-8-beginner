e3e265e842d6733822bda62dd1c66b6cac492dc2
DevDotKP <59239430+DevDotKP@users.noreply.github.com>
2020-04-02 11:13:12 +0530

    Create List_Duplicate_removal.java

---
+++ List_Duplicate_removal.java
@@ -0,0 +1,28 @@
+package Java_8;
+
+ 
+
+import java.util.Arrays;
+
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+ 
+
+public class RemoveDuplicatesFromList {
+
+                public static void main(String args[]) {
+
+                                Integer a[] = {3,2,3,1,2,5,4,7,8,9,6,5,4,6,7,8,6,4,3,2,3,1,2,4,1,2};
+
+                                List<Integer> integerList = Arrays.asList(a);
+
+                               
+
+                                System.out.println("Array with unique and ordered values: " + integerList.stream().distinct().sorted().collect(Collectors.toList()));
+
+                }
+
+}
+
